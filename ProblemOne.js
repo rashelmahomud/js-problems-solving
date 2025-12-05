92e19b77b31f78bbd889b9d97e34e1991002e1cd
rashelmahomud <rashelmahmudraj1998@gmail.com>
2025-12-05 18:29:54 +0600

    added

---
+++ ProblemOne.js
@@ -0,0 +1,119 @@
+console.log("Hello World !");
+
+/* 
+problems=01 
+
+const a = 5;
+const b = 7;
+
+ const result = a+b;
+ console.log(result) 
+ 
+ */
+
+//===============================
+
+//find data type
+
+/*
+problems- 02
+const x = 10;
+ console.log(typeof x)
+
+ */
+
+//-==============================
+
+/*
+problems- 03
+ convert String to Number
+
+ const y = '10';
+ console.log(y)
+ console.log(Number(y))
+
+*/
+
+//--================================
+
+//awap to variables
+//problems- = 4
+/*
+let a = 5;
+let b = 4;
+[a, b] = [b, a];
+
+console.log(a, b);
+
+*/
+
+//======================
+
+//problems = 5
+
+/*
+let n= 99;
+
+const res = n % 2 == 0 ? "Enen" : "Odd";
+console.log(res)
+
+*/
+
+//======================
+//problems = 6
+/*
+
+const n = 100
+console.log(n)
+const res = String(n);
+console.log(res);
+
+*/
+
+//======================
+//problems = 7
+
+/*
+simple interest
+const p = 1000;
+let r = 5;
+let t = 2;
+
+const res = (p * r * t) / 100;
+console.log(res);
+
+*/
+
+//======================
+//problems = 8
+
+/*
+
+let a = 5;
+let b= 8;
+
+const res = (a*b)
+console.log(res);
+
+
+*/
+
+//======================
+//problems = 9
+/*
+Temperature Celsius → Fahrenheit
+let c = 30;
+const res = (c * 9) / 5 + 32;
+
+console.log(res)
+*/
+
+//==============================
+// problems- 10
+
+/*reverse a string 
+let str = "hello";
+
+const tes = str.split('').reverse().join("")
+console.log(tes)*/
+
